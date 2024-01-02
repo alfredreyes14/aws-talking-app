84da245fe0f8066b352834934147f05c8b3d294d
John Alfred Reyes <jtan896@gmail.com>
2024-01-02 15:37:00 +0800

    refactor: extracted s3 logic to different function

--- backend/handler.js
+++ backend/handler.js
@@ -3,6 +3,39 @@ let polly = new AWS.Polly();
 let s3 = new AWS.S3();
 const { v4: uuidv4 } = require('uuid')
 
+const createOutputFileInS3 = ({ params, bucketName, key, callback }) => {
+  s3.putObject(params)
+    .on("success", function (response) {
+      console.log("S3 Put Success!");
+    })
+    .on("complete", function () {
+      console.log("S3 Put Complete!");
+      let s3params = {
+        Bucket: bucketName,
+        Key: key + '.mp3',
+      };
+      // 3. Getting a signed URL for the saved mp3 file 
+      let url = s3.getSignedUrl("getObject", s3params);
+      // Sending the result back to the user
+      let result = {
+        bucket: bucketName,
+        key: key + '.mp3',
+        url
+      };
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin" : "*"
+        },
+        body: JSON.stringify(result)
+      });
+    })
+    .on("error", function (response) {
+      console.log(response);
+    })
+    .send();
+}
+
 module.exports.speak = (event, context, callback) => {
   let data = JSON.parse(event.body);
   const pollyParams = {
@@ -16,43 +49,19 @@ module.exports.speak = (event, context, callback) => {
       let data = response.data;
       let audioStream = data.AudioStream;
       let key = uuidv4();
-      let s3BucketName = process.env.BUCKET_NAME;  
+      let bucketName = process.env.BUCKET_NAME;  
       // 2. Saving the audio stream to S3
       let params = {
-        Bucket: s3BucketName,
+        Bucket: bucketName,
         Key: key + '.mp3',
         Body: audioStream
       };
-      s3.putObject(params)
-        .on("success", function (response) {
-          console.log("S3 Put Success!");
-        })
-        .on("complete", function () {
-          console.log("S3 Put Complete!");
-          let s3params = {
-            Bucket: s3BucketName,
-            Key: key + '.mp3',
-          };
-          // 3. Getting a signed URL for the saved mp3 file 
-          let url = s3.getSignedUrl("getObject", s3params);
-          // Sending the result back to the user
-          let result = {
-            bucket: s3BucketName,
-            key: key + '.mp3',
-            url: url
-          };
-          callback(null, {
-            statusCode: 200,
-            headers: {
-              "Access-Control-Allow-Origin" : "*"
-            },
-            body: JSON.stringify(result)
-          });
-        })
-        .on("error", function (response) {
-          console.log(response);
-        })
-        .send();
+      createOutputFileInS3({
+        params,
+        bucketName,
+        key,
+        callback
+      })
     })
     .on("error", function (err) {
       callback(null, {
